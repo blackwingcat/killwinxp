1d4c13c1a489a7e51c08bdead5cc4bba9af835c4
BlackWingCat <blackwingcat2000@gmail.com>
2023-04-27 19:10:29 +0900

    関連ファイル

---
+++ framework.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+// Windows ヘッダー ファイル
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <random>
+#include <stdio.h>
